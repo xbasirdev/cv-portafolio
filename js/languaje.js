0a7092c2bd65add9642742bf7567b6f16489d193
Xavier Basir <xavierfiv@gmail.com>
2019-08-16 17:21:38 -0400

    ampliando descripcion de proyectos en la seccion de portafolio

---
+++ js/languaje.js
@@ -0,0 +1,12 @@
+function inSpanish() {
+    document.getElementById("title").innerHTML = "Xavier Basir - Desarrollador web";
+    document.getElementById("skills").innerHTML = "Habilidades";
+
+}
+
+function inEnglish() {
+    document.getElementById("title").innerHTML = "Xavier Basir - Web developer";
+    
+    document.getElementById("skills").innerHTML = "Skills";
+
+}
